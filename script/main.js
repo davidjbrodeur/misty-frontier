ca39f652f590c458639095afe0f4340144eccc0c
dbrodeur <davidjbrodeur@gmail.com>
2022-06-24 20:48:15 -0400

    Finished intro section. Started character section. Created table template.

--- script/main.js
+++ script/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
     $("#header").load("pages/header.html");
     $("#footer").load("pages/footer.html");
+    $("#stats-table").load("pages/stats.html");
+    $("#example-charsheet").load("pages/example_charsheet.html");
+    $("#injury-table").load("pages/injury_table.html");
 });
